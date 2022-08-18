cddb6c4aa6f48af56de7dd962998eca183d43b9d
Alan Lin <alan.lin@gapp.nthu.edu.tw>
2022-08-19 01:34:22 +0800

    Initialize fake temperature reader server. Need to change the json reponse type.

---
+++ read-only-sensor.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import random
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<p>This site is a prototype API for read-only sensor.</p>"
+
+
+@app.route('/status', methods=['GET'])
+def getTemperature():
+    temperature = random.uniform(25.0, 39.9)
+    ret = json.dumps(temperature)
+    print(ret)
+    return ret
+
+app.run()
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Sample Folight server API readme
+
+Setting up Flask to make your simple server.
+
+### Start a read only sensor example.
+```
+$ python3 -m venv venv
+$ source venv/bin/activate
+(venv) $ python3 -m pip install requirements.txt
+(venv) $ python3 read-only-sensor.py
+```
