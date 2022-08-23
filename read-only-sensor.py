c679d2d5bddfa40be6e4e20222c54309a55d9cf1
Alan Lin <alan.lin@gapp.nthu.edu.tw>
2022-08-24 06:30:44 +0800

    Fix into read-only sensor to correct respose json type.

--- read-only-sensor.py
+++ read-only-sensor.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
 import random
 import json
 
 app = Flask(__name__)
+unique_id = 0x1111
 
 @app.route('/', methods=['GET'])
 def home():
@@ -12,8 +13,13 @@ def home():
 @app.route('/status', methods=['GET'])
 def getTemperature():
     temperature = random.uniform(25.0, 39.9)
-    ret = json.dumps(temperature)
-    print(ret)
-    return ret
+    response = Response()
+    response.status_code = 200
+    response.data = json.dumps({
+        "unique_id": unique_id,
+        "temperature": temperature # Define more content here
+    })
+    print(response.data)
+    return response
 
 app.run(host='0.0.0.0', port=80)
