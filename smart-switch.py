0c365a6da51aae5c503a21d63f4d2f1aa9b61df1
alanlin-nthu <111528847+alanlin-nthu@users.noreply.github.com>
2022-08-25 21:55:07 +0800

    Modify sensor URL and indent and readme (#2)
    * Switch API read=GET write=POST. Adding mimetype application/json such that client could know it's json.
    
    * Change sensor URL route from status to sensor_status.
    
    Indent and readme.md update

--- smart-switch.py
+++ smart-switch.py
@@ -4,71 +4,72 @@ import json
 
 app = Flask(__name__)
 
+
 class Switch:
+    # Here define the GPIO or sensor reading.
+
     def __init__(self, value):
-        self._switch = value  
-    def getEnable(self):
+        self._switch = value
+
+    def get_enable(self):
         return self._switch
-    def setEnable(self, enable):
+
+    def set_enable(self, enable):
         self._switch = enable
         return True
 
 
 currentSwitch = Switch(False)  # Default switch is false
 
+
 @app.route('/', methods=['GET'])
 def home():
     return "<p>This site is a prototype API for smart switch.</p>"
 
 
-@app.route('/read_switch', methods=['POST'])
+@app.route('/smart_switch', methods=['GET', 'POST'])
 def get_status():
-    if request.method == "POST":
-        response = Response()
-
-        if (type(currentSwitch.getEnable()) == Boolean): # Switch state normal case
+    if request.method == "GET":
+        response = Response(mimetype='application/json')
+        if type(currentSwitch.get_enable()) == Boolean:  # Switch state normal case
             print("server 200")
             response.status_code = 200
             response.data = json.dumps({
-                "switch_enable": currentSwitch.getEnable() # Define more content here
+                "is_enable": currentSwitch.get_enable()  # Define more content here
             })
-
-        else:                                            # Switch state abnormal case
+        else:  # Switch state abnormal case
             print("server 500")
             response.status_code = 500
             response.data = json.dumps({
-                "err": "Check your switch availability cannot read" # Handle more error here
+                "err": "Check your switch availability cannot read"  # Handle more error here
             })
 
         print(response)
         return response
 
-@app.route('/write_switch', methods=['POST'])
-def set_status():
     if request.method == "POST":
-        response = Response()
-        set_data = json.loads(request.get_data(as_text=True))        
-        
-        set_result = currentSwitch.setEnable(set_data['enable'])
+        response = Response(mimetype='application/json')
+        set_data = request.get_json()
+
+        set_result = currentSwitch.set_enable(set_data['enable'])
         is_valid_type = type(set_data['enable']) == Boolean
         is_success = set_result & is_valid_type
 
-        if (is_success): # Switch state normal case
+        if is_success:  # Switch state normal case
             print("server 200")
             response.status_code = 200
             response.data = json.dumps({
-                "switch_enable": currentSwitch.getEnable(),          # Define more content here
+                "is_enable": currentSwitch.get_enable(),  # Define more content here
             })
-
-        else:            # Switch state abnormal case
+        else:  # Switch state abnormal case
             print("server 500")
             response.status_code = 500
             response.data = json.dumps({
-                "err": "Check your switch availability cannot write" # Handle more error here
+                "err": "Check your switch availability cannot write"  # Handle more error here
             })
 
         print(response)
         return response
 
 
-app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=8091)
